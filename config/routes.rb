ed4101123dfd65e0b7317518d19f5d1deadd5507
jorge-Coke <jorge.barra1412@gmail.com>
2020-03-02 00:56:52 -0300

    categories images added

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
 
   resources :activities
   resources :categories do
     delete 'delete/:image_id', on: :member, to: 'categories#destroy_image', as: 'delete_image'
   end
   
-  devise_for :users
-  
   get 'pages/index'
-  
-
-  
+    
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
