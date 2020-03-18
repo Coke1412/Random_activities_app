adc50108eef7c5f59a6a2d0bee2e1dcb42ab0f7b
jorge-Coke <jorge.barra1412@gmail.com>
2020-03-17 21:10:31 -0300

    chart style changed

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   # devise_scope :user do
   #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   # end
+
+
   
   get 'pages/index'
     
