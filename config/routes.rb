54b9f0e40d58bd54b195d9a22c9221819d77bb20
jorge-Coke <jorge.barra1412@gmail.com>
2020-03-19 03:49:03 -0300

    responsive design done. Footer added.

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,18 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  resources :pages
   resources :activities
   resources :categories do
     delete 'delete/:image_id', on: :member, to: 'categories#destroy_image', as: 'delete_image'
   end
 
-  # devise_scope :user do
-  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # end
+  #resources :choices
+
 
 
   
-  get 'pages/index'
+  
     
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
