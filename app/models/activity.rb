3500bf3bbf3798c88eb5c71bb993bc58bdc56471
jorge-Coke <jorge.barra1412@gmail.com>
2020-03-20 11:38:28 -0300

    minor changes

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,11 +1,12 @@
 class Activity < ApplicationRecord
     belongs_to :category
     has_and_belongs_to_many :users
-    #si borro la actividad se borran los usuarios de la relación
+    #if activity is destroyed, related users too
 
     has_one_attached :image
 
     validates :name, presence: true
+    validates :name, uniqueness: true
     validates :image, presence: true
     validates :category_id, presence: true
     
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,9 @@
 class Category < ApplicationRecord
     has_many :activities, dependent: :destroy
-    #si borro las categorías se borran las actividades
+    
     has_one_attached :image
+    
+    validates :name, presence: true
     validates :name, presence: true
     validates :description, presence: true
     
